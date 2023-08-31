c30a59a19bc1d89127689921742ebeeffc07fbca
Said Berk <csaidberk@gmail.com>
2023-08-31 23:00:10 +0300

    Sign In completed. CSS added to navbar.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,23 @@
+// Navbar.js
 import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import './Navbar.css'; // Import the CSS file
+
 export const Navbar = () => {
-    return(
-        <div>
-            <Link to = "/">Home </Link> {/*<a> yerine <link> kullanılmasının sebebi sayfayı yenilemeden navigasyon yapılması */}
-            <Link to = "/login"> Login</Link>
-        </div>
-    );
-}
+  const [user] = useAuthState(auth);
+  const username = auth.currentUser?.displayName;
+
+  return (
+    <div className="navbar">
+      <div>
+        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/login" className="nav-link" >Login</Link>
+      </div>
+      <div className="user-info">
+        <p>{username}</p>
+        <img src={auth.currentUser?.photoURL || ""} alt="User" />
+      </div>
+    </div>
+  );
+};
